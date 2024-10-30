19d7f942da02d4e297b696989c26bfbf1cccfa44
Antony Chalkley <antonychalkley@hotmail.co.uk>
2024-10-30 19:07:17 +0000

    Add the list of services to the side of the carosel and added functionality to hover over the list item to display the carosel item

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,16 @@
 document.addEventListener("DOMContentLoaded", function() {
-    var cards = document.querySelectorAll("[id^=card-]");
+    const cards = document.querySelectorAll("[id^=card-]");
     cards.forEach(function(card) {
         card.addEventListener("mouseover", cardMouseOver);
         card.addEventListener("mouseout", cardMouseOut);
         card.addEventListener("click", cardClick);
+    });
+
+    const serviceButtons =  document.querySelectorAll("[class^=service-buttons]")
+        serviceButtons.forEach(function(button) {
+        button.addEventListener("mouseover", buttonMouseOver);
+        button.addEventListener("mouseout", buttonMouseOut);
+        // button.addEventListener("click", buttonClick);
     });
 });
 
@@ -23,4 +30,15 @@ function cardMouseOut(e) {
 
 function cardClick(e){
     location.replace("/gallery.html");
+}
+
+function buttonMouseOver(e){
+    let button = e.currentTarget
+    button.click();
+    button.classList.add("active")
+}
+
+function buttonMouseOut(e){
+    let button = e.currentTarget
+    button.classList.remove("active")
 }
